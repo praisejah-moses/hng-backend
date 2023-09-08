e2c5343ac91c95c8ecaa78c0c5e197ff3aa2a28b
PraiseJah Moses <praisejah.moses22@gmail.com>
2023-09-08 11:19:41 +0100

    fix utc time

--- stage_1/src/controller.js
+++ stage_1/src/controller.js
@@ -9,8 +9,8 @@ export const stageOneEndpoint = (req,res)=>{
             res.send(
                 {
                     "slack_name":req.query.slack_name,
-                    "current_day":date.getDay,
-                    "utc_time":date.getUTCHours()+":"+date.getUTCMinutes()+":"+date.getUTCSeconds(),
+                    "current_day":date.getDay(),
+                    "utc_time":Date.UTC(date.getUTCFullYear(),date.getUTCMonth(),date.getUTCDate,date.getUTCHours(),date.getUTCMinutes,date.getUTCSeconds),
                     "track":req.query.track,
                     "github_file_url":"https://github.com/praisejah-moses/hng-backend/blob/main/stage_1/app.js",
                     "github_repo_url":"https://github.com/praisejah-moses/hng-backend/tree/main/stage_1",
