3cfa5060a3a5f230d9c3d0bc2a23fc0bd2edec42
PraiseJah Moses <praisejah.moses22@gmail.com>
2023-09-08 20:56:39 +0100

    fixed milliesec in utc format

--- stage_1/src/controller.js
+++ stage_1/src/controller.js
@@ -7,13 +7,14 @@ export const stageOneEndpoint = (req,res)=>{
         const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
         const date = new Date() 
         const day = weekday[date.getDay()];
+        const utc_time = date.toISOString().split('.')[0]+"Z"
 
         if(req.query.slack_name && req.query.track){
-            res.send(
+            res.status(200).json(
                 {
                     "slack_name":req.query.slack_name,
                     "current_day":day,
-                    "utc_time":date.toISOString(),
+                    "utc_time":utc_time,
                     "track":req.query.track,
                     "github_file_url":"https://github.com/praisejah-moses/hng-backend/blob/main/stage_1/app.js",
                     "github_repo_url":"https://github.com/praisejah-moses/hng-backend/tree/main/stage_1",
@@ -27,7 +28,7 @@ export const stageOneEndpoint = (req,res)=>{
         }}
     catch(error){
         console.log(error)
-        res.send({
+        res.status(500).json({
             'status_code':500
         })
     }
