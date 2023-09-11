aea370fc0e92243ebedf764769d5115f9e505834
PraiseJah Ossai <118202914+praisejah-moses@users.noreply.github.com>
2023-09-11 23:01:41 +0100

    Delete stage_1 directory

--- stage_1/src/route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import { root, stageOneEndpoint } from "./controller.js"
-
-const router = Router()
-
-router.get('/',root)
-router.get('/api',stageOneEndpoint)
-
-export default router;
